f7a23a8a3732479d87dce06f37692959a590eef5
sameerbjs <105976062+sameerbjs@users.noreply.github.com>
2023-05-09 01:43:04 +0500

    avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { Link, NavLink } from "react-router-dom";
 import logo from '../../assets/images/res-logo.png'
+import avatar from '../../assets/images/ava-3.jpg'
 import { useSelector } from "react-redux";
 
 function Header() {
@@ -66,10 +67,11 @@ function Header() {
                                 </Link>
                                 <Popover className="relative mt-2" as="div">
                                     <Popover.Button>
-                                        <div>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7 cursor-pointer">
+                                        <div className="w-8 h-8">
+                                            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7 cursor-pointer">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                                            </svg>
+                                            </svg> */}
+                                            <img src={avatar} alt="avatar" className="w-full rounded-3xl object-cover object-center h-full" />
                                         </div>
                                     </Popover.Button>
                                     <Transition
